6c1499fddf03d69a0fd0462254e36a23c3c63ff6
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-03-24 06:38:08 +0000

    ssssssssss

--- src/components/main/SaleItemCard.jsx
+++ src/components/main/SaleItemCard.jsx
@@ -7,34 +7,31 @@ const SaleItemCard = ({ id, imageUrl, title, price, item_description }) => {
   const cardBg = useColorModeValue('white', 'gray.800');
 
   return (
-    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" bg={cardBg}>
-      <Box position="relative">
-        <Image src={imageUrl} alt={title} borderRadius="lg" borderTopRadius="lg" borderBottomRadius="0" />
-        <Box position="absolute" top="2" right="2" display="flex" alignItems="center">
-          <IconButton
-            aria-label={`Like ${title}`}
-            icon={<FaHeart />}
-            size="sm"
-            colorScheme="teal"
-            variant="solid"
-            borderRadius="full"
-          />
+    <Link to={`/${id}`} style={{ textDecoration: 'none' }}>
+      <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" bg={cardBg} _hover={{ boxShadow: "md" }} cursor="pointer">
+        <Box position="relative">
+          <Image src={imageUrl} alt={title} borderRadius="lg" borderTopRadius="lg" borderBottomRadius="0" />
+          <Box position="absolute" top="2" right="2" display="flex" alignItems="center">
+            <IconButton
+              aria-label={`Like ${title}`}
+              icon={<FaHeart />}
+              size="sm"
+              colorScheme="teal"
+              variant="solid"
+              borderRadius="full"
+            />
+          </Box>
+        </Box>
+
+        <Box ml="3">
+          <Flex direction="column">
+            <Text fontSize="lg">{title}</Text>
+            <Text fontSize="lg">{price} €</Text>
+            <Text fontSize="sm" mt="2">{item_description}</Text>
+          </Flex>
         </Box>
       </Box>
-
-      <Box ml="3">
-        <Flex direction="column">
-          <Text fontSize="lg">{title}</Text>
-          <Text fontSize="lg">{price} €</Text>
-          <Text fontSize="sm" mt="2">{item_description}</Text>
-        </Flex>
-        <Link to={`/${id}`} style={{ textDecoration: 'none' }}>
-          <Text mt="2" color="blue.400" fontSize="sm" fontWeight="bold">
-            View Details
-          </Text>
-        </Link>
-      </Box>
-    </Box>
+    </Link>
   );
 };
 
