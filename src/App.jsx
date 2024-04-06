1072c8e703f54ab886614ff343eb631a293643ad
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-06 17:42:10 +0000

    erytertetere

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,59 @@
-// App.jsx
-import React from "react";
-import {
-  ChakraProvider,
-  Flex,
-  Box,
-  useDisclosure,
-  Drawer,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
-  DrawerHeader,
-  DrawerBody,
-  IconButton,
-} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { ChakraProvider, Box, Flex, IconButton, useDisclosure, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import ItemsForSaleSupabase from "./components/main/ItemForSaleSupabase";
 import FullWidthBanner from "./components/header/FullWidthBanner";
 import Headerb from "./components/Headerb";
 import HeaderBar from "./components/HeaderBar";
-import ItemDetail from "./components/main/ItemDetail";
-import routesConfig, { createDynamicRoutes } from './routes/routesConfig';
 import LazyComponentWrapper from './LazyComponentWrapper';
+import createDynamicRoutes from './routes/routesConfig';
 
 const App = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [categoriesWithSubs, setCategoriesWithSubs] = useState([]);
 
-  // Logic to fetch categories from Supabase can be placed here or inside useEffect
-  const categoriesWithSubs = []; // Assume this is fetched and formatted as needed
+
 
   const dynamicRoutes = createDynamicRoutes(categoriesWithSubs);
-  const combinedRoutes = [...routesConfig, ...dynamicRoutes];
+
+  const combinedRoutes = [...dynamicRoutes];
 
   return (
     <ChakraProvider>
       <Router>
-      <HeaderBar />
+        <HeaderBar />
         <Headerb />
-        {/* Navbar only visible on larger screens */}
         <Box display={{ base: 'none', md: 'block' }}>
           <Navbar />
         </Box>
         <FullWidthBanner />
-        {/* Mobile Nav Button */}
-        <IconButton
-          icon={<HamburgerIcon />}
-          onClick={onOpen}
-          display={{ base: "inline-flex", md: "none" }}
-          aria-label="Open menu"
-          position="absolute"
-          top={4}
-          left={4}
-        />
-        {/* Mobile Drawer */}
+        <IconButton icon={<HamburgerIcon />} onClick={onOpen} display={{ base: "inline-flex", md: "none" }} aria-label="Open menu" position="absolute" top={4} left={4} />
         <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
           <DrawerOverlay />
           <DrawerContent>
             <DrawerCloseButton />
             <DrawerHeader>Menu</DrawerHeader>
             <DrawerBody>
-              {/* Navbar for small screens, within the Drawer */}
               <Navbar isDrawer={true} />
             </DrawerBody>
           </DrawerContent>
         </Drawer>
         <Flex>
-          {/* Main Content */}
           <Box flex="1" p={5}>
-          <Routes>
-              {/* ... more routes */}
+            <Routes>
+              {combinedRoutes.map((route, index) => (
+                <Route
+                  key={index}
+                  path={route.path}
+                  element={
+                    React.createElement(LazyComponentWrapper(route.component), {
+                      ...route.data,
+                      key: index,
+                    })
+                  }
+                />
+              ))}
             </Routes>
           </Box>
         </Flex>
--- src/routes/routesConfig.jsx
+++ src/routes/routesConfig.jsx
@@ -1,11 +1,12 @@
-  // Add this function to routesConfig.js
-export const createDynamicRoutes = (categoriesWithSubs) => {
-    const dynamicRoutes = categoriesWithSubs.flatMap(category => [
-      { path: `/${encodeURIComponent(category.name.toLowerCase())}`, component: 'CategoryPage', data: category },
-      ...category.subcategories.map(sub => ({
-        path: `/subcategory/${encodeURIComponent(sub.name.toLowerCase())}`, component: 'SubcategoryPage', data: sub
-      }))
-    ]);
-  
-    return dynamicRoutes;
-  };
+const createDynamicRoutes = (categoriesWithSubs) => {
+  const dynamicRoutes = categoriesWithSubs.flatMap(category => [
+    { path: `/${encodeURIComponent(category.name.toLowerCase())}`, component: 'CategoryPage', data: category },
+    ...category.subcategories.map(sub => ({
+      path: `/subcategory/${encodeURIComponent(sub.name.toLowerCase())}`, component: 'SubcategoryPage', data: sub
+    }))
+  ]);
+
+  return dynamicRoutes;
+};
+
+export default createDynamicRoutes;
