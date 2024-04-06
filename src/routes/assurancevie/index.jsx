45a131c513ef42bfc54ddaa541e34f7cd26d141b
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-06 17:45:05 +0000

    zfffezerer

---
+++ src/routes/assurancevie/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import FeatureBox from './component/FeatureBox'; // Importez FeatureBox.jsx
+
+const AssuranceVie = () => {
+  return (
+    <Box>
+      <h1>Assurance Vie Page</h1>
+      <FeatureBox />
+    </Box>
+  );
+};
+
+export default AssuranceVie;
