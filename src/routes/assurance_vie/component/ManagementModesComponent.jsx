32262b317a77cf973f0f09d5ee5b6d58b60a109c
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-07 10:48:12 +0000

    eeeeeeeeeeeeeee

--- src/routes/assurance_vie/component/ManagementModesComponent.jsx
+++ src/routes/assurance_vie/component/ManagementModesComponent.jsx
@@ -4,6 +4,8 @@ import {
     Heading,
     HStack,
     Icon,
+    Grid,
+    GridItem,
     List,
     ListItem,
     ListIcon,
@@ -15,54 +17,55 @@ import {
   import { MdCheckCircle } from 'react-icons/md';
   
   function ManagementModesComponent() {
-    const stackDirection = useBreakpointValue({ base: 'column', md: 'row' });
-  
     return (
       <Container maxW="container.xl" centerContent py={10}>
-        <VStack
-          divider={<StackDivider borderColor="gray.200" />}
-          spacing={4}
-          align="stretch"
-          direction={stackDirection}
-        >
-          <HStack spacing={3} justify="center">
-            <Icon as={FcManager} w={10} h={10} />
-            <Heading size="md">Gestion libre</Heading>
-          </HStack>
-          <List spacing={2} mt={2}>
-            <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Autonomie: Sélectionnez vous-même vos supports d'investissement
-            </ListItem>
-            <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Liberté: Arbitrez sans frais quand vous le souhaitez pour un contrat toujours en adéquation avec vos projets
-            </ListItem>
-            <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Conseil: Bénéficiez à tout moment d’un coup de pouce dans votre sélection grâce aux allocations stars Linxea et à nos conseillers en investissement financier
-            </ListItem>
-          </List>
+        <Grid templateColumns={{ md: 'repeat(2, 1fr)' }} gap={6}>
+          <GridItem>
+            <VStack spacing={4} align="center">
+              <HStack justify="center" w="full">
+                <Icon as={FcManager} w={10} h={10} />
+                <Heading size="md" textAlign="center">Gestion libre</Heading>
+              </HStack>
+              <List spacing={2} textAlign="center">
+                <ListItem>
+                  <ListIcon as={MdCheckCircle} color="green.500" />
+                  Autonomie: Sélectionnez vous-même vos supports d'investissement
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={MdCheckCircle} color="green.500" />
+                  Liberté: Arbitrez sans frais quand vous le souhaitez pour un contrat toujours en adéquation avec vos projets
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={MdCheckCircle} color="green.500" />
+                  Conseil: Bénéficiez à tout moment d’un coup de pouce dans votre sélection grâce aux allocations stars Linxea et à nos conseillers en investissement financier
+                </ListItem>
+              </List>
+            </VStack>
+          </GridItem>
   
-          <HStack spacing={3} justify="center">
-            <Icon as={FcAutomatic} w={10} h={10} />
-            <Heading size="md">Gestion pilotée</Heading>
-          </HStack>
-          <List spacing={2} mt={2}>
-            <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Tranquillité: Déléguez la gestion financière de votre contrat un professionnel de la finance
-            </ListItem>
-            <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Cohérence: Bénéficiez d'allocations en accord avec votre profil de risque et vos objectifs de performances
-            </ListItem>
-            <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Suivi: Consultez les reportings réguliers où le gérant détaille sa stratégie et explique ses choix d'arbitrage.
-            </ListItem>
-          </List>
-        </VStack>
+          <GridItem>
+            <VStack spacing={4} align="center">
+              <HStack justify="center" w="full">
+                <Icon as={FcAutomatic} w={10} h={10} />
+                <Heading size="md" textAlign="center">Gestion pilotée</Heading>
+              </HStack>
+              <List spacing={2} textAlign="center">
+                <ListItem>
+                  <ListIcon as={MdCheckCircle} color="green.500" />
+                  Tranquillité: Déléguez la gestion financière de votre contrat un professionnel de la finance
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={MdCheckCircle} color="green.500" />
+                  Cohérence: Bénéficiez d'allocations en accord avec votre profil de risque et vos objectifs de performances
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={MdCheckCircle} color="green.500" />
+                  Suivi: Consultez les reportings réguliers où le gérant détaille sa stratégie et explique ses choix d'arbitrage.
+                </ListItem>
+              </List>
+            </VStack>
+          </GridItem>
+        </Grid>
       </Container>
     );
   }
