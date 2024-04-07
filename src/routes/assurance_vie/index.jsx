3d3fd5863fce218aff5f7fd4ec23daa7e7e59c32
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-07 06:18:40 +0000

    thyghyrftrttr

--- src/routes/assurance_vie/index.jsx
+++ src/routes/assurance_vie/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, Text, Button, VStack, Image } from '@chakra-ui/react';
+import { Box, Flex, Text, Button, VStack, Image, Icon } from '@chakra-ui/react';
 import { FiCheckCircle } from 'react-icons/fi';
 
 // SVG Line Chart
@@ -9,7 +9,8 @@ const MiniChart = () => {
       src="public/images/agavic/undraw_growth_analytics_re_pyxf.svg"
       alt="Growth Analytics"
       borderRadius="md"
-      mb={4}
+      mb={2} // Ajuster la marge inférieure pour réduire l'espace entre le SVG et les autres composants
+      maxHeight="150px" // Définir une hauteur maximale pour réduire la taille du SVG
     />
   );
 };
@@ -17,9 +18,9 @@ const MiniChart = () => {
 // Simulation Button and Text Component
 const SimulationButton = () => {
   return (
-    <VStack align="center" p={8} spacing={6} bg="#FFA500" borderRadius="md" width="300px" boxShadow="lg">
-      <Box as={FiCheckCircle} boxSize="48px" color="white" />
-      <Text fontWeight="bold" fontSize="2xl">L'assurance-vie :</Text>
+    <VStack align="center" p={8} spacing={6} bg="#FFD700" borderRadius="xl" width="300px" boxShadow="lg">
+      <Icon as={FiCheckCircle} boxSize="48px" color="white" />
+      <Text fontWeight="bold" fontSize="2xl">L'assurance-vie 💼</Text>
       <Text fontSize="lg" textAlign="center">
         Nous sélectionnons pour vous les meilleurs contrats du marché : des frais bas et un large choix de supports
       </Text>
@@ -31,7 +32,7 @@ const SimulationButton = () => {
 // Main AssuranceVie component
 const AssuranceVie = () => {
   return (
-    <Flex direction={{ base: 'column', md: 'row' }} align="center" justify="space-around" p={8} bg="#F7FAFC" borderRadius="md" boxShadow="lg">
+    <Flex direction={{ base: 'column', md: 'row' }} align="center" justify="space-around" p={8} bg="#F7FAFC" borderRadius="xl" boxShadow="xl">
       <SimulationButton />
       <MiniChart />
     </Flex>
