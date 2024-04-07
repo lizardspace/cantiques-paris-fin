c1a980d1e6922025920d39c3456bc3acc4bd1b06
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-07 14:46:06 +0000

    hgfjhgkj

--- src/routes/assurance_vie/component/InsuranceRisksComponent.jsx
+++ src/routes/assurance_vie/component/InsuranceRisksComponent.jsx
@@ -1,27 +1,25 @@
-import { Box, Flex, Heading, Text, VStack, HStack, Icon } from '@chakra-ui/react';
-// If you're using React Icons, import them like this:
-// import { GiMoneyStack, ... } from 'react-icons/gi';
+import { Box, Flex, Heading, Text, VStack, HStack } from '@chakra-ui/react';
+import { FcAutomatic, FcApproval, FcCurrencyExchange, FcTax } from "react-icons/fc";
 
 function InsuranceRisksComponent() {
-  // This could be your data structure, you can replace it with actual icons and text.
   const risks = [
     {
-      icon: /* Your transferability icon here */,
+      icon: FcAutomatic,
       title: 'Transférabilité limitée :',
       description: 'Transfert possible uniquement au sein d’une même compagnie d’assurance'
     },
     {
-      icon: /* Your capital icon here */,
+      icon: FcApproval,
       title: 'Capital et rendement non garantis',
       description: 'Risque de perte en capital en cas d’investissement en unités de compte'
     },
     {
-      icon: /* Your fees icon here */,
+      icon: FcCurrencyExchange,
       title: 'Frais applicables',
       description: 'Des frais de gestion annuels s’appliquent ainsi que certains frais de transaction en fonction des supports et des contrats'
     },
     {
-      icon: /* Your tax icon here */,
+      icon: FcTax,
       title: 'Fiscalité',
       description: 'Fiscalité applicable en fonction de la date des versements et de l’âge du contrat.'
     }
@@ -35,7 +33,7 @@ function InsuranceRisksComponent() {
       <HStack justifyContent="space-between" alignItems="flex-start">
         {risks.map((risk, index) => (
           <VStack key={index} p={4} spacing={3} align="center">
-            <Icon as={risk.icon} w={10} h={10} color="orange.500" />
+            {<risk.icon size={30} color="orange" />} {/* Utilisation des crochets pour évaluer l'expression */}
             <Text fontWeight="bold">{risk.title}</Text>
             <Text textAlign="center">{risk.description}</Text>
           </VStack>
