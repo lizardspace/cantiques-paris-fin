bc8e90f9205be3e6277da121bab3930b5459bb18
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-07 05:21:23 +0000

    kjhbkj

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Heading, Flex, Image } from '@chakra-ui/react';
 
 const NotFound = () => {
   return (
     <Box textAlign="center" py={10} px={6}>
-      <Text fontSize="18px" mt={3} mb={2}>
-        Page Not Found
+      <Image src="/images/agavic/undraw_page_not_found_re_e9o6.svg" alt="Page non trouvée" maxW="400px" mx="auto" />
+      <Heading as="h1" fontSize="3xl" mt={6} mb={4}>
+        Page non trouvée
+      </Heading>
+      <Text fontSize="xl" color="gray.600" mb={6}>
+        Désolé, la page que vous recherchez n'existe pas.
       </Text>
-      <Text color={'gray.500'}>
-        Sorry, the page you are looking for does not exist.
-      </Text>
+      <Flex justify="center">
+        <Text fontSize="lg" color="blue.500" textDecoration="underline">
+          <a href="/">Retour à la page d'accueil</a>
+        </Text>
+      </Flex>
     </Box>
   );
 };
