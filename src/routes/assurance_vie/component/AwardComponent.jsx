75504c7e2575aeeaebb6df7f67d1dff752c9cb40
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-07 15:35:15 +0000

    ssssssssssd

--- src/routes/assurance_vie/component/AwardComponent.jsx
+++ src/routes/assurance_vie/component/AwardComponent.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
-import { Box, Flex, Image, Text, Badge } from '@chakra-ui/react';
-import { StarIcon } from '@chakra-ui/icons';
+import { Box, Flex, Text, Icon, Badge, VStack, HStack } from '@chakra-ui/react';
+import { GiTrophyCup } from 'react-icons/gi';
+import { BsStarFill } from 'react-icons/bs';
 
 const AwardComponent = () => {
   return (
-    <Flex alignItems="center" justifyContent="space-around" p={5} bg="white" boxShadow="md">
-      <Box>
-        <Image src="/path-to-your-first-award-image.png" alt="Award 1" boxSize="100px" />
-        <Text fontSize="sm" textAlign="center">France Awards PER 2023</Text>
-      </Box>
-      <Box>
-        <Image src="/path-to-your-second-award-image.png" alt="Award 2" boxSize="100px" />
-        <Text fontSize="sm" textAlign="center">Challenger Meilleurs PER 2023</Text>
-      </Box>
-      <Box>
-        <Image src="/path-to-your-third-award-image.png" alt="Award 3" boxSize="100px" />
-        <Text fontSize="sm" textAlign="center">PER Trophées d'Or 2023</Text>
-      </Box>
-      <Flex direction="column" alignItems="center" justifyContent="center">
-        <Badge colorScheme="green" mb={2}>100 000 clients nous font confiance</Badge>
-        <Badge colorScheme="orange" mb={2}>3,3 milliards d'euros confiés</Badge>
-        <Badge colorScheme="yellow" mb={2}>20 ans de conception de produits d'épargne</Badge>
-        <Flex alignItems="center">
-          <StarIcon color="yellow.400" />
+    <Flex
+      wrap="wrap"
+      alignItems="center"
+      justifyContent="center"
+      p={5}
+      bg="white"
+      boxShadow="md"
+      textAlign="center"
+    >
+      <VStack m={4}>
+        <Icon as={GiTrophyCup} w={20} h={20} color="yellow.400" />
+        <Text fontSize="sm">France Awards PER 2023</Text>
+      </VStack>
+      
+      <VStack m={4}>
+        <Icon as={GiTrophyCup} w={20} h={20} color="gray.400" />
+        <Text fontSize="sm">Challenger Meilleurs PER 2023</Text>
+      </VStack>
+      
+      <VStack m={4}>
+        <Icon as={GiTrophyCup} w={20} h={20} color="green.400" />
+        <Text fontSize="sm">PER Trophées d'Or 2023</Text>
+      </VStack>
+      
+      <VStack m={4} alignItems="center" justifyContent="center">
+        <Badge colorScheme="green" mb={2} p={2}>100 000 clients nous font confiance</Badge>
+        <Badge colorScheme="orange" mb={2} p={2}>3,3 milliards d'euros confiés</Badge>
+        <Badge colorScheme="yellow" mb={2} p={2}>20 ans de conception de produits d'épargne</Badge>
+        <HStack alignItems="center">
+          <Icon as={BsStarFill} w={10} h={10} color="yellow.400" />
           <Text ml={2}>4,3/5 sur Trustpilot</Text>
-        </Flex>
-      </Flex>
+        </HStack>
+      </VStack>
     </Flex>
   );
 }
