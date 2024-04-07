0628cb769b9fb20ab8bbb6632392845b4f34e4d4
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-07 20:22:16 +0000

    dfssfdsdsdsdsf

--- src/components/Footer/FooterComponent.jsx
+++ src/components/Footer/FooterComponent.jsx
@@ -5,7 +5,7 @@ const FooterComponent = () => {
   return (
     <Box backgroundColor="orange.300" paddingY="4">
       <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} alignItems="center" justifyContent="center">
-        <Box textAlign={{ base: 'center', md: 'initial' }}>
+        <Box textAlign={{ base: 'center', md: 'initial' }} p={5} >
           <Text fontWeight="bold" marginBottom="4">Conseiller en Investissements Financiers (CIF)</Text>
           <Text marginBottom="2">Membre de la CNCIF, association professionnelle agréée par l'Autorité des Marchés Financiers (AMF).</Text>
           <Text marginBottom="2">Enregistré à l'ORIAS en tant que Courtier en Assurance, activité régulée par l'Autorité de Contrôle Prudentiel et de Résolution (ACPR).</Text>
