02142e5b6adefabafc8b66bf20efe036e8c8219a
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-07 20:14:20 +0000

    ffffffffffe

--- src/components/Footer/FooterComponent.jsx
+++ src/components/Footer/FooterComponent.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
-import { Box, Link, SimpleGrid, Text } from '@chakra-ui/react';
+import { Box, Link, SimpleGrid, Text, Center } from '@chakra-ui/react';
 
-// Assuming you want to make this a separate component
 const FooterComponent = () => {
   return (
-    <Box backgroundColor="orange.300" paddingY="4" paddingX="8">
-      <SimpleGrid columns={5} spacing={10}>
-        <Box>
-          <Text fontWeight="bold">Conseiller en Investissements Financiers (CIF)</Text>
-          <Text>Membre de la CNCIF, association professionnelle agréée par l'Autorité des Marchés Financiers (AMF).</Text>
-          <Text>Enregistré à l'ORIAS en tant que Courtier en Assurance, activité régulée par l'Autorité de Contrôle Prudentiel et de Résolution (ACPR).</Text>
+    <Box backgroundColor="orange.300" paddingY="4">
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} alignItems="center" justifyContent="center">
+        <Box textAlign={{ base: 'center', md: 'initial' }}>
+          <Text fontWeight="bold" marginBottom="4">Conseiller en Investissements Financiers (CIF)</Text>
+          <Text marginBottom="2">Membre de la CNCIF, association professionnelle agréée par l'Autorité des Marchés Financiers (AMF).</Text>
+          <Text marginBottom="2">Enregistré à l'ORIAS en tant que Courtier en Assurance, activité régulée par l'Autorité de Contrôle Prudentiel et de Résolution (ACPR).</Text>
         </Box>
-        <Box as="nav" aria-label="Footer navigation">
-          <Link href="#" color="white" isExternal>
-            Conditions générales d'utilisation
-          </Link>
-          <Link href="#" color="white" isExternal marginTop="2">
-            Données personnelles
-          </Link>
-          <Link href="#" color="white" isExternal marginTop="2">
-            Mentions légales
-          </Link>
-          <Link href="#" color="white" isExternal marginTop="2">
-            Cookies
-          </Link>
-        </Box>
+        <Center>
+          <Box>
+            <Text fontWeight="bold" marginBottom="4">Liens utiles :</Text>
+            <Box as="nav" aria-label="Footer navigation">
+              <Link href="#" color="white" isExternal marginBottom="2" display="block">
+                Conditions générales d'utilisation
+              </Link>
+              <Link href="#" color="white" isExternal marginBottom="2" display="block">
+                Données personnelles
+              </Link>
+              <Link href="#" color="white" isExternal marginBottom="2" display="block">
+                Mentions légales
+              </Link>
+              <Link href="#" color="white" isExternal marginBottom="2" display="block">
+                Cookies
+              </Link>
+            </Box>
+          </Box>
+        </Center>
       </SimpleGrid>
     </Box>
   );
