342a1f92b3d66ec2c6d7f231e3f5668f1121a4e1
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-07 07:44:22 +0000

    trghyrtyrytt

--- src/routes/assurance_vie/component/SavingsSimulator.jsx
+++ src/routes/assurance_vie/component/SavingsSimulator.jsx
@@ -10,7 +10,14 @@ import {
   Grid,
   Heading,
   VStack,
+  Flex,
+  IconButton,
+  Tooltip,
+  Image,
+  Card,
+  CardBody,
 } from '@chakra-ui/react';
+import { FaMoneyBillWave, FaCalendarAlt, FaPercentage } from 'react-icons/fa';
 
 export default function SavingsSimulator() {
   const [initialDeposit, setInitialDeposit] = useState(10000);
@@ -20,90 +27,142 @@ export default function SavingsSimulator() {
 
   return (
     <Box p={8}>
-      <VStack spacing={8}>
-        <Heading as="h1" size="xl" textAlign="center">
+      <VStack spacing={8} align="center">
+        <Heading as="h1" size="xl">
           Simulateur d'épargne
         </Heading>
-        <Text>Un projet d'épargne? En fonction de votre capacité, simulez différents scénarios d'épargne.</Text>
+        <Text textAlign="center">Un projet d'épargne? En fonction de votre capacité, simulez différents scénarios d'épargne.</Text>
 
         <Grid templateColumns="repeat(4, 1fr)" gap={6} width="100%">
           <Box>
-            <Text>Versement initial:</Text>
-            <Slider
-              aria-label="slider-ex-1"
-              defaultValue={initialDeposit}
-              min={1000}
-              max={300000}
-              step={1000}
-              onChange={(val) => setInitialDeposit(val)}
-            >
-              <SliderMark value={initialDeposit} textAlign="center" bg="orange.300" color="white" mt="-10" ml="-5" w="12">
-                {initialDeposit}€
-              </SliderMark>
-              <SliderTrack>
-                <SliderFilledTrack bg="orange.300" />
-              </SliderTrack>
-              <SliderThumb boxSize={6} />
-            </Slider>
+            <Card>
+              <CardBody>
+                <Flex align="center">
+                  <IconButton
+                    aria-label="Initial Deposit"
+                    icon={<FaMoneyBillWave />}
+                    variant="outline"
+                    size="lg"
+                    mr={2}
+                  />
+                  <Text fontSize="lg" fontWeight="bold">Versement initial</Text>
+                </Flex>
+                <Slider
+                  aria-label="slider-ex-1"
+                  defaultValue={initialDeposit}
+                  min={1000}
+                  max={300000}
+                  step={1000}
+                  onChange={(val) => setInitialDeposit(val)}
+                >
+                  <SliderMark value={initialDeposit} textAlign="center" bg="orange.300" color="white" mt="-10" ml="-5" w="12">
+                    {initialDeposit}€
+                  </SliderMark>
+                  <SliderTrack>
+                    <SliderFilledTrack bg="orange.300" />
+                  </SliderTrack>
+                  <SliderThumb boxSize={6} />
+                </Slider>
+              </CardBody>
+            </Card>
           </Box>
 
           <Box>
-            <Text>Versements mensuels:</Text>
-            <Slider
-              aria-label="slider-ex-2"
-              defaultValue={monthlyDeposit}
-              min={0}
-              max={1500}
-              step={50}
-              onChange={(val) => setMonthlyDeposit(val)}
-            >
-              <SliderMark value={monthlyDeposit} textAlign="center" bg="orange.300" color="white" mt="-10" ml="-5" w="12">
-                {monthlyDeposit}€
-              </SliderMark>
-              <SliderTrack>
-                <SliderFilledTrack bg="orange.300" />
-              </SliderTrack>
-              <SliderThumb boxSize={6} />
-            </Slider>
+            <Card>
+              <CardBody>
+                <Flex align="center">
+                  <IconButton
+                    aria-label="Monthly Deposit"
+                    icon={<FaMoneyBillWave />}
+                    variant="outline"
+                    size="lg"
+                    mr={2}
+                  />
+                  <Text fontSize="lg" fontWeight="bold">Versements mensuels</Text>
+                </Flex>
+                <Slider
+                  aria-label="slider-ex-2"
+                  defaultValue={monthlyDeposit}
+                  min={0}
+                  max={1500}
+                  step={50}
+                  onChange={(val) => setMonthlyDeposit(val)}
+                >
+                  <SliderMark value={monthlyDeposit} textAlign="center" bg="orange.300" color="white" mt="-10" ml="-5" w="12">
+                    {monthlyDeposit}€
+                  </SliderMark>
+                  <SliderTrack>
+                    <SliderFilledTrack bg="orange.300" />
+                  </SliderTrack>
+                  <SliderThumb boxSize={6} />
+                </Slider>
+              </CardBody>
+            </Card>
           </Box>
 
           <Box>
-            <Text>Durée de l'investissement:</Text>
-            <Slider
-              aria-label="slider-ex-3"
-              defaultValue={investmentDuration}
-              min={1}
-              max={20}
-              onChange={(val) => setInvestmentDuration(val)}
-            >
-              <SliderMark value={investmentDuration} textAlign="center" bg="orange.300" color="white" mt="-10" ml="-5" w="12">
-                {investmentDuration} ans
-              </SliderMark>
-              <SliderTrack>
-                <SliderFilledTrack bg="orange.300" />
-              </SliderTrack>
-              <SliderThumb boxSize={6} />
-            </Slider>
+            <Card>
+              <CardBody>
+                <Flex align="center">
+                  <IconButton
+                    aria-label="Investment Duration"
+                    icon={<FaCalendarAlt />}
+                    variant="outline"
+                    size="lg"
+                    mr={2}
+                  />
+                  <Text fontSize="lg" fontWeight="bold">Durée de l'investissement</Text>
+                </Flex>
+                <Slider
+                  aria-label="slider-ex-3"
+                  defaultValue={investmentDuration}
+                  min={1}
+                  max={20}
+                  onChange={(val) => setInvestmentDuration(val)}
+                >
+                  <SliderMark value={investmentDuration} textAlign="center" bg="orange.300" color="white" mt="-10" ml="-5" w="12">
+                    {investmentDuration} ans
+                  </SliderMark>
+                  <SliderTrack>
+                    <SliderFilledTrack bg="orange.300" />
+                  </SliderTrack>
+                  <SliderThumb boxSize={6} />
+                </Slider>
+              </CardBody>
+            </Card>
           </Box>
 
           <Box>
-            <Text>Rendement de votre épargne:</Text>
-            <Slider
-              aria-label="slider-ex-4"
-              defaultValue={returnRate}
-              min={0.5}
-              max={5}
-              step={0.1}
-              onChange={(val) => setReturnRate(val)}
-            >
-              <SliderMark value={returnRate} textAlign="center" bg="orange.300" color="white" mt="-10" ml="-5" w="12">
-                {returnRate}%/an
-              </SliderMark>
-              <SliderTrack>
-                <SliderFilledTrack bg="orange.300" />
-              </SliderTrack>
-              <SliderThumb boxSize={6} />
-            </Slider>
+            <Card>
+              <CardBody>
+                <Flex align="center">
+                  <IconButton
+                    aria-label="Return Rate"
+                    icon={<FaPercentage />}
+                    variant="outline"
+                    size="lg"
+                    mr={2}
+                  />
+                  <Text fontSize="lg" fontWeight="bold">Rendement de votre épargne</Text>
+                </Flex>
+                <Slider
+                  aria-label="slider-ex-4"
+                  defaultValue={returnRate}
+                  min={0.5}
+                  max={5}
+                  step={0.1}
+                  onChange={(val) => setReturnRate(val)}
+                >
+                  <SliderMark value={returnRate} textAlign="center" bg="orange.300" color="white" mt="-10" ml="-5" w="12">
+                    {returnRate}%/an
+                  </SliderMark>
+                  <SliderTrack>
+                    <SliderFilledTrack bg="orange.300" />
+                  </SliderTrack>
+                  <SliderThumb boxSize={6} />
+                </Slider>
+              </CardBody>
+            </Card>
           </Box>
         </Grid>
       </VStack>
