e3ebf13f770d9c778578a883427052deeb12cfd8
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-07 15:13:03 +0000

    rfezrereze

--- src/routes/assurance_vie/component/InsuranceAccordion.jsx
+++ src/routes/assurance_vie/component/InsuranceAccordion.jsx
@@ -10,33 +10,32 @@ import {
   Text
 } from '@chakra-ui/react';
 
-// Définir les données pour chaque section de l'accordéon
-const accordionData = [
-  {
-    title: "Introduction à l'assurance vie",
-    description: "Ajoutez ici la description de l'introduction à l'assurance vie."
-  },
-  {
-    title: "Principe de fonctionnement",
-    description: "Ajoutez ici la description du principe de fonctionnement de l'assurance vie."
-  },
-  {
-    title: "Le cadre fiscal avantageux de l'assurance vie",
-    description: "Ajoutez ici la description du cadre fiscal avantageux de l'assurance vie."
-  },
-  {
-    title: "Quels sont les frais de l'assurance vie ?",
-    description: "Ajoutez ici la description des frais de l'assurance vie."
-  },
-  {
-    title: "Halte aux idées reçues sur l’assurance vie",
-    description: "Ajoutez ici la description pour combattre les idées reçues sur l'assurance vie."
-  },
-  {
-    title: "Questions fréquentes sur l'assurance vie",
-    description: "Ajoutez ici la description des questions fréquentes sur l'assurance vie."
-  }
-];
+    const accordionData = [
+        {
+          title: "Introduction à l'assurance vie",
+          description: "Découvrez les bases de l'assurance vie, un outil financier polyvalent et indispensable pour préparer l'avenir. Cette section explore les multiples facettes de l'assurance vie, de son utilité dans la planification financière à ses avantages fiscaux attrayants."
+        },
+        {
+          title: "Principe de fonctionnement",
+          description: "Plongez dans le fonctionnement interne de l'assurance vie et comprenez comment cette forme d'investissement vous permet de protéger votre avenir financier. Explorez les mécanismes qui régissent l'assurance vie, des primes aux bénéficiaires, et découvrez comment elle peut être adaptée à vos besoins spécifiques."
+        },
+        {
+          title: "Le cadre fiscal avantageux de l'assurance vie",
+          description: "Explorez les avantages fiscaux exceptionnels offerts par l'assurance vie. Découvrez comment cette forme d'épargne peut vous aider à optimiser votre situation fiscale, en minimisant les impôts sur les gains et les transmissions. Cette section vous éclairera sur les stratégies fiscales intelligentes à adopter avec l'assurance vie."
+        },
+        {
+          title: "Quels sont les frais de l'assurance vie ?",
+          description: "Démystifiez les frais associés à l'assurance vie et apprenez à les comprendre pour prendre des décisions financières éclairées. Cette section examine les différents frais liés à l'assurance vie, des frais de gestion aux frais d'entrée, et vous aide à évaluer leur impact sur vos investissements."
+        },
+        {
+          title: "Halte aux idées reçues sur l’assurance vie",
+          description: "Débarrassez-vous des idées fausses et des préjugés sur l'assurance vie. Cette section démystifie les mythes courants entourant l'assurance vie, comme son prétendu manque de liquidité ou son utilité limitée. Découvrez la vérité sur l'assurance vie et comment elle peut vraiment être un pilier solide de votre stratégie financière."
+        },
+        {
+          title: "Questions fréquentes sur l'assurance vie",
+          description: "Trouvez des réponses à vos questions les plus fréquentes sur l'assurance vie. Cette section aborde les préoccupations courantes des souscripteurs d'assurance vie, des modalités de paiement des primes aux options de gestion des bénéficiaires. Obtenez des éclaircissements pour prendre des décisions éclairées sur votre avenir financier."
+        }
+      ];
 
 function InsuranceAccordion() {
   return (
