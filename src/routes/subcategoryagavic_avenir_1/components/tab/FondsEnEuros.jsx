85dae938aac9ffb48f92be0dfd014df09b06f8f7
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-08 21:05:13 +0000

    dfsdsfdfsddf

--- src/routes/subcategoryagavic_avenir_1/components/tab/FondsEnEuros.jsx
+++ src/routes/subcategoryagavic_avenir_1/components/tab/FondsEnEuros.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, Text, VStack, Button } from '@chakra-ui/react';
+import { Box, Text, VStack, Button, useColorModeValue, Icon } from '@chakra-ui/react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { MdInfoOutline } from 'react-icons/md';
 
 const data = [
   { name: '2023', uv: 2.5 },
@@ -14,18 +15,19 @@ const data = [
 const CustomTooltip = ({ active, payload, label }) => {
   if (active) {
     return (
-      <Box p="2" bg="white" boxShadow="md">
+      <Box p="2" bg="white" boxShadow="md" borderRadius="md">
         <Text fontWeight="bold">{`${label}`}</Text>
         <Text>{`Rendement : ${payload[0].value}%`}</Text>
         {payload[1] && <Text>{`Bonus : ${payload[1].value}%`}</Text>}
       </Box>
     );
   }
-
   return null;
 };
 
 const FondsEnEuros = () => {
+  const textColor = useColorModeValue('gray.700', 'gray.200');
+
   return (
     <VStack
       p={4}
@@ -34,21 +36,23 @@ const FondsEnEuros = () => {
       borderRadius="lg"
       align="center"
       spacing={4}
-      width="100%" // Adjust to fit your layout needs
-      maxWidth="100%" // This sets the maximum width the VStack can take
+      width="100%"
+      maxWidth="100%"
     >
       <Text fontSize="lg" fontWeight="bold">
         Objectif 4,50% net de frais de gestion en 2024
       </Text>
-      <Text color="green.500" fontSize="sm" mb={4}>
-        +2% sur le rendement du fonds en euros Suravenir Opportunités 2
-      </Text>
+      <Box d="flex" alignItems="center">
+        <Icon as={MdInfoOutline} mr={1} color="green.500" />
+        <Text color="green.500" fontSize="sm">
+          +2% sur le rendement du fonds en euros Suravenir Opportunités 2
+        </Text>
+      </Box>
       <ResponsiveContainer width="100%" height={250}>
         <BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
           <XAxis dataKey="name" tickLine={false} axisLine={false} />
           <YAxis hide={true} />
           <Tooltip content={<CustomTooltip />} />
-          {/* Adjust barSize to control the width of the bars */}
           <Bar dataKey="uv" stackId="a" fill="#FFA500" barSize={300} radius={[0, 0, 0, 0]} />
           <Bar dataKey="bonus" stackId="a" fill="#32CD32" barSize={100} radius={[10, 10, 0, 0]} />
         </BarChart>
@@ -56,7 +60,7 @@ const FondsEnEuros = () => {
       <Button colorScheme="teal" size="sm">
         Découvrir l'offre
       </Button>
-      <Text fontSize="xs">
+      <Text fontSize="xs" textAlign="center" fontWeight="bold" color={textColor}>
         Du 27/01/2024 au 31/12/2024, Suravenir applique une bonification...
         {/* Insert the rest of the text here */}
       </Text>
