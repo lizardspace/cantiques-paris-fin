e581adcdc608e860220667ea3e744fb4ee6ce1e0
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-04-09 18:48:49 +0000

    dddddddff

--- src/routes/subcategoryagavic_avenir_1/components/tab/InvestmentManagementComponent.jsx
+++ src/routes/subcategoryagavic_avenir_1/components/tab/InvestmentManagementComponent.jsx
@@ -4,7 +4,7 @@ import { Box, Table, Thead, Tbody, Tr, Th, Td, Text } from '@chakra-ui/react';
 const InvestmentManagementComponent = () => {
   return (
     <Box overflowX="auto">
-      <Table variant="striped" colorScheme="teal">
+      <Table variant="striped" colorScheme="blue">
         <Thead>
           <Tr>
             <Th>Gestion libre</Th>
@@ -14,36 +14,43 @@ const InvestmentManagementComponent = () => {
         <Tbody>
           <Tr>
             <Td>
-              <Text fontWeight="bold">Autonomie:</Text>
+              <Text fontWeight="bold" color="#3182ce">Autonomie:</Text>
               Sélectionnez vous-même vos supports d'investissement...
             </Td>
             <Td>
-              <Text fontWeight="bold">Tranquillité:</Text>
+              <Text fontWeight="bold" color="#3182ce">Tranquillité:</Text>
               Déléguez la gestion financière de votre contrat...
             </Td>
           </Tr>
           <Tr>
             <Td>
-              <Text fontWeight="bold">Liberté:</Text>
+              <Text fontWeight="bold" color="#3182ce">Liberté:</Text>
               Arbitrez sans frais quand vous le souhaitez...
             </Td>
             <Td>
-              <Text fontWeight="bold">Cohérence:</Text>
+              <Text fontWeight="bold" color="#3182ce">Cohérence:</Text>
               Bénéficiez d'allocations en accord avec votre profil...
             </Td>
           </Tr>
           <Tr>
             <Td>
-              <Text fontWeight="bold">Conseil:</Text>
+              <Text fontWeight="bold" color="#3182ce">Conseil:</Text>
               Bénéficiez à tout moment d'un coup de pouce...
             </Td>
             <Td>
-              <Text fontWeight="bold">Suivi:</Text>
+              <Text fontWeight="bold" color="#3182ce">Suivi:</Text>
               Consultez les reportings réguliers où le gérant détaille...
             </Td>
           </Tr>
         </Tbody>
       </Table>
+      <style>
+        {`
+          .chakra-table tbody tr:nth-of-type(odd) {
+            background-color: #e5f2ff; // Light blue color
+          }
+        `}
+      </style>
     </Box>
   );
 };
